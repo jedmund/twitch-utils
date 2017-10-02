ed79e3cb831da8b9ebdff59e60f30a3024bf6883
Justin Edmund <justin@jedmund.com>
2017-10-02 15:42:31 -0700

    Updated App component to display our hierarchy

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from '../../images/logo.svg';
+import Page from 'containers/Page/Page';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+          <Page></Page>
       </div>
     );
   }
